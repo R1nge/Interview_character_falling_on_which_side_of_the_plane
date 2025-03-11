0107cfb3c2d05aa444bc74e5ca5c0d28e4fe8a24
R1nge <r1ngegamedev@gmail.com>
2025-03-11 12:24:15 +0300

    feat: detect relative position

---
+++ Assets/Scripts/RelativePositionDetector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class RelativePositionDetector : MonoBehaviour
+{
+    [SerializeField] private Transform character;
+    [SerializeField] private Transform[] corners;
+
+    private void Update()
+    {
+        Detect();
+    }
+
+    private void Detect()
+    {
+        if (IsAboveThePlane())
+        {
+            Debug.Log("Character is above the plane");
+        }
+        else
+        {
+            Debug.Log("Character is below the plane");
+        }
+    }
+
+    private bool IsAboveThePlane()
+    {
+        foreach (var corner in corners)
+        {
+            if (GetSign(corner.position.y, character.position.y) > 0)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    private float GetSign(float corner, float character)
+    {
+        var signDistance = character - corner;
+        return signDistance;
+    }
+}
